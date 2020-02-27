56ed6087db4c283907730c7577745fbc983f6b91
Hoang Anh <lequochoang@live.com>
2020-02-27 09:21:35 +0700

    update cau hỏi

--- src/pages/home/question.js
+++ src/pages/home/question.js
@@ -8,47 +8,37 @@ var questions = [
             'Câu thơ gây ấn tượng mạnh bởi sự kết hợp của hình ảnh khách quan và cảm nhận chủ quan.',
             'Hình ảnh thơ kết hợp giữa hiện thực và lãng mạn.'
         ],
-        answer: 4
-    },
-    {
-        name: 'Nhận định nào không đúng khi nói về tác giả của bài thơ “Thuật hoài”?',
-        chooses: [
-            'Người làng Phù Ủng, huyện Đường hào, nay thuộc Ân Thi, Hưng Yên.',
-            'Là con rể nuôi của Trần Quốc Tuấn.',
-            'Có nhiều công lao trong cuộc kháng chiến chống quân Nguyên.',
-            'Là một nhà quan văn, nên ông thích ngâm thơ đọc sách.'
-        ],
-        answer: 4
-    },
-    {
-        name: 'Từ nào dưới đây trong bài thơ “Thuật hoài” không phải là tên con vật?',
-        chooses: [
-            'Sóc.',
-            'Tì.',
-            'Hổ.',
-            'Ngưu.'
-        ],
         answer: 1
-    },
+    }, 
     {
-        name: 'Trong những nhận xét sau đây, nhận xét nào không đúng khi nói về câu thơ thứ hai trong bài “Thuật hoài”?',
+        name: 'Tình cảm, cảm xúc nào không được thể hiện trong bài thơ "Thuật hoài"?',
         chooses: [
-            'Tam quân là ba người lính, đồng thời cũng có thể hiểu là ba đạo quân.',
-            'Hình ảnh ba quân nói về quân đội nhưng cũng đồng thời nói về sức mạnh của toàn dân tộc.',
-            'Câu thơ gây ấn tượng mạnh bởi sự kết hợp của hình ảnh khách quan và cảm nhận chủ quan.',
-            'Hình ảnh thơ kết hợp giữa hiện thực và lãng mạn.'
-        ],
-        answer: 1
-    },
-    {
-        name: 'Phạm Ngũ Lão cảm thấy thẹn khi nghe người đời kể chuyện ai?',
-        chooses: [
-            'Lưu Bị.',
-            'Tào Tháo.',
-            'Quan Công.',
-            'Gia Cát Lượng.'
+            'Tự hào về khí thế và sức mạnh của quân đội thời Trần.',
+            'Thẹn vì chưa trả xong nợ công danh.',
+            'Tình yêu nước, tự hào dân tộc.',
+            'Phê phán triều đình phong kiến.'
         ],
         answer: 4
+    },
+    {
+        name: 'Đặc sắc nghệ thuật trong bài thơ “Thuật hoài” là gì?',
+        chooses: [
+            'Nhân hoá.',
+            'Ẩn dụ.',
+            'Quý hồ tinh bất quý hồ đa.',
+            'Kể chuyện.'
+        ],
+        answer: 2
+    },
+    {
+        name: 'Cảm hứng yêu nước được thể hiện qua những tác phẩm nào sau đây?',
+        chooses: [
+            'Nam quốc sơn hà, Thuật hoài, Truyện Kiều.',
+            'Nhàn, Bình Ngô đại cáo, Thuật hoài.',
+            'Thuật hoài, Nam quốc sơn hà, Chiếu dời đô.',
+            'Thuật hoài, Nam quốc sơn hà, Độc Tiểu Thanh kí.'
+        ],
+        answer: 3
     }
 ]
 
