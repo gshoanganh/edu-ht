9cf2d6b2eef14b8cdadc1deda184366634bb5d03
Hoang Anh <lequochoang@live.com>
2020-02-27 09:30:51 +0700

    update question

--- src/pages/home/question.js
+++ src/pages/home/question.js
@@ -1,6 +1,51 @@
 
 var questions = [
     {
+        name: 'Trong những nhận xét sau đây, nhận xét nào không đúng về câu thơ thứ hai trong bài "Thuật hoài"?',
+        chooses: [
+            'Tam quân là ba người lính, đồng thời cũng có thể hiểu là ba đạo quân.',
+            'Hình ảnh ba quân thể hiện sức mạnh của toàn dân tộc.',
+            'Câu thơ gây ấn tượng mạnh bởi sự kết hợp của hình ảnh khách quan và cảm nhận chủ quan.',
+            'Hình ảnh thơ kết hợp giữa hiện thực và lãng mạn.'
+        ],
+        answer: 1
+    }, 
+    {
+        name: 'Tình cảm, cảm xúc nào không được thể hiện trong bài thơ "Thuật hoài"?',
+        chooses: [
+            'Tự hào về khí thế và sức mạnh của quân đội thời Trần.',
+            'Thẹn vì chưa trả xong nợ công danh.',
+            'Tình yêu nước, tự hào dân tộc.',
+            'Phê phán triều đình phong kiến.'
+        ],
+        answer: 4
+    },
+    {
+        name: 'Đặc sắc nghệ thuật trong bài thơ “Thuật hoài” là gì?',
+        chooses: [
+            'Nhân hoá.',
+            'Ẩn dụ.',
+            'Quý hồ tinh bất quý hồ đa.',
+            'Kể chuyện.'
+        ],
+        answer: 2
+    },
+    {
+        name: 'Cảm hứng yêu nước được thể hiện qua những tác phẩm nào sau đây?',
+        chooses: [
+            'Nam quốc sơn hà, Thuật hoài, Truyện Kiều.',
+            'Nhàn, Bình Ngô đại cáo, Thuật hoài.',
+            'Thuật hoài, Nam quốc sơn hà, Chiếu dời đô.',
+            'Thuật hoài, Nam quốc sơn hà, Độc Tiểu Thanh kí.'
+        ],
+        answer: 3
+    }
+]
+
+export {questions};
+
+var questions1 = [
+    {
         name: 'Tình cảm, cảm xúc nào không được thể hiện trong bài thơ “Thuật hoài”?',
         chooses: [
             'Tự hào về khí thế và sức manh của quân đội thời trần.',
